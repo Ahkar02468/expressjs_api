951c7312adcadf0b661cf04cc21bb11d7889c84a
Ahkar02468 <arkarshwebaw@gmail.com>
2024-07-24 01:12:39 +0700

    done adding middle http methods

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,23 @@
-import express, { query } from 'express';
+import express, { query, response } from 'express';
 const app = express();
 app.use(express.json())
+//order matters in middleware case
+const loginMiddleware = (request, response, next) => {
+     console.log(`${request.method} - ${request.url}`)
+     next()
+}
+
+const userResolveToGetId = (request, response, next) => {
+     const { params: {id}} = request;
+     const parseId = parseInt(id);
+     if(isNaN(parseId)) return response.status(400).send({msg: 'Bad request. Invalid ID.'});
+     const getIndexToUpdate = mockUsers.findIndex(user => user.id === parseId);
+     if(getIndexToUpdate === -1) return response.sendStatus(404);
+     request.getIndexToUpdate = getIndexToUpdate;
+     next()
+}
+
+// app.use(loginMiddleware)
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,10 +25,11 @@ const mockUsers = [
      {id:1, name: 'john doe', displayName: 'JD'},
      {id:2, name: 'Black Jack', displayName: 'jack'},
      {id:3, name: 'Tom Hanks', displayName: 'Tom'},
-     {id:4, name: 'Jones Herry', displayName: 'Jones'}
+     {id:4, name: 'Jones Herry', displayName: 'Jones'},
+     {id:5, name: 'Kelly James', displayName: 'Kelly'}
 ]
 
-app.get('/', (request, response) => {
+app.get('/', loginMiddleware, (request, response) => {
      response.status(201).send({message: 'hello jsosn'})
 })
 
@@ -38,42 +56,28 @@ app.get('/api/products', (request, response) => {
      ])
 })
 
-app.get('/api/users/:id', (request, response) => {
-     console.log(request.params)
-     const parseId = parseInt(request.params.id)
-     if(isNaN(parseId)) return response.status(400).send({msg: 'Bad request. Invalid ID.'})
-     const user = mockUsers.find(user => user.id === parseId)
+app.get('/api/users/:id',userResolveToGetId, (request, response) => {
+     const { getIndexToUpdate } = request
+     const user = mockUsers[getIndexToUpdate]
      if(!user) return response.sendStatus(404)
      return response.send(user)
 })
 
-app.put('/api/users/:id', (request, response) => {
-     const { params: {id}, body } = request;
-     const parseId = parseInt(id)
-     if(isNaN(parseId)) return response.status(400).send({msg: 'Bad request. Invalid ID.'});
-     const getIndexToUpdate = mockUsers.findIndex(user => user.id === parseId);
-     if(getIndexToUpdate === -1) return response.sendStatus(404);
-     mockUsers[getIndexToUpdate] = {id: parseId, ...body};
+app.put('/api/users/:id',userResolveToGetId, (request, response) => {
+     const { body, getIndexToUpdate } = request
+     mockUsers[getIndexToUpdate] = {id: mockUsers[getIndexToUpdate].id, ...body};
      response.status(200).send(mockUsers[getIndexToUpdate]);
 })
 
-app.patch('/api/users/:id', (request, response) => {
-     const { params: {id}, body } = request;
-     const parseId = parseInt(id)
-     if(isNaN(parseId)) return response.status(400).send({msg: 'Bad request. Invalid ID.'});
-     const getIndexToUpdate = mockUsers.findIndex(user => user.id === parseId);
-     if(getIndexToUpdate === -1) return response.sendStatus(404);
+app.patch('/api/users/:id',userResolveToGetId, (request, response) => {
+     const { body, getIndexToUpdate } = request;
      mockUsers[getIndexToUpdate] = {...mockUsers[getIndexToUpdate], ...body}
      response.status(200).send(mockUsers[getIndexToUpdate])
 })
 
-app.delete('/api/users/:id', (request, response) => {
-     const {params: {id}} = request
-     const parseId = parseInt(id)
-     if(isNaN(parseId)) return response.status(400).send({msg: "Bad request, Invalid ID."})
-     const getIdtoUpdate = mockUsers.findIndex(user => user.id === parseId)
-     if(getIdtoUpdate === -1) return response.sendStatus(404)
-     mockUsers.splice(getIdtoUpdate, 1)
+app.delete('/api/users/:id',userResolveToGetId, (request, response) => {
+     const {getIndexToUpdate} = request
+     mockUsers.splice(getIndexToUpdate, 1)
      return response.sendStatus(200)
 })
 
